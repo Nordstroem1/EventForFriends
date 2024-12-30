8e28e3760eb269a2f5fd9112071ec59e32f63b2a
Nordstroem1 <johannord27@hotmail.com>
2024-12-30 12:35:03 +0100

    wip

---
+++ Domain/Models/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models
+{
+    public class Comment
+    {
+        [Required]
+        public Guid CommentId { get; set; }
+        [Required]
+        [MaxLength(700)]
+        public string CommentContent { get; set; }
+        public DateTime TimeSent { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public Guid EventId { get; set; }
+        public Comment() { }
+        public Comment(Guid commentId, string content, DateTime createdAt, Guid userId, Guid eventId)
+        {
+            CommentId = commentId;
+            CommentContent = content;
+            TimeSent = createdAt;
+            UserId = userId;
+            EventId = eventId;
+        }
+}
+}
