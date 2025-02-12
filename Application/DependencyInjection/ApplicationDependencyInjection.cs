2b48da85222c556b5fe064ea102cef0202ad784b
Nordstroem1 <johannord27@hotmail.com>
2025-02-12 20:35:38 +0100

    identity working with jwt

--- Application/DependencyInjection/ApplicationDependencyInjection.cs
+++ Application/DependencyInjection/ApplicationDependencyInjection.cs
@@ -16,31 +16,6 @@ namespace Application.DependencyInjection
         {
             var assembly = typeof(ApplicationDependencyInjection).Assembly;
 
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("SuperAdmin", policy =>
-                {
-                    policy.AuthenticationSchemes.Add(IdentityConstants.ApplicationScheme);
-                    policy.RequireAuthenticatedUser();
-                    policy.RequireRole("SuperAdmin");
-                });
-                options.AddPolicy("Admin", policy =>
-                {
-                    policy.AuthenticationSchemes.Add(IdentityConstants.ApplicationScheme);
-                    policy.RequireAuthenticatedUser();
-                    policy.RequireRole("Admin");
-                });
-                options.AddPolicy("User", policy =>
-                {
-                    policy.AuthenticationSchemes.Add(IdentityConstants.ApplicationScheme);
-                    policy.RequireAuthenticatedUser();
-                    policy.RequireRole("User");
-                });
-            });
-
-            services.AddDbContext<mySqlDb>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-
             services.AddMediatR(config => config.RegisterServicesFromAssemblies(assembly));
             services.AddScoped<TokenHelper>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
